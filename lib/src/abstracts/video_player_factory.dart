d7069af5aec537e31da2b97a19d10a151b46cb9a
Bart Ribbers <bart@iconica.nl>
2023-01-16 09:41:19 +0100

    Fix an incorrect import on web

--- lib/src/abstracts/video_player_factory.dart
+++ lib/src/abstracts/video_player_factory.dart
@@ -7,7 +7,7 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_media_picker/video_player/video_player_native.dart'
-    if (dart.library.html) 'package:media_picker/video_player/video_player_web.dart'
+    if (dart.library.html) 'package:flutter_media_picker/video_player/video_player_web.dart'
     as vp;
 import 'package:video_player/video_player.dart';
 
